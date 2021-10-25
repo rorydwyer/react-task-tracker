b2c202a8d2c98f999d3b5904bf36367164789fed
Rory <rory.dwy@gmail.com>
2021-10-25 08:36:08 -0700

    Add toggle reminder functionality for server

--- src/App.js
+++ src/App.js
@@ -1,40 +1,81 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 function App() {
   const [showAddTasks, setShowAddTasks] = useState(false);
+  const [tasks, setTasks] = useState([]);
 
-  const [tasks, setTasks] = useState([
-    { id: 1, text: "Task 1", day: "Monday", reminder: false },
-    { id: 2, text: "Task 2", day: "Tuesday", reminder: false },
-    { id: 3, text: "Task 3", day: "Wednesday", reminder: false },
-    { id: 4, text: "Task 4", day: "Thursday", reminder: false },
-    { id: 5, text: "Task 5", day: "Friday", reminder: false },
-    { id: 6, text: "Task 6", day: "Saturday", reminder: false },
-  ]);
+  // SEVER-LIKE FUNCTIONALITY
+  // LifeCycle Hook
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    };
+    getTasks();
+  }, []);
 
+  // Fetch tasks from server
+  const fetchTasks = async () => {
+    const res = await fetch("http://localhost:3001/tasks");
+    const data = await res.json();
+    return data;
+  };
+
+  // Fetch single task from server
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:3001/tasks/${id}`);
+    const data = await res.json();
+    return data;
+  };
+
+  // TASK METHODS
   // Toggle Add Task Form
   const toggleAddTask = () => {
     setShowAddTasks(!showAddTasks);
   };
 
   // Add task
-  const addTask = ({ text, day, reminder }) => {
-    setTasks([...tasks, { id: tasks.length + 1, text, day, reminder }]);
+  const addTask = async (task) => {
+    // setTasks([...tasks, { id: tasks.length + 1, text, day, reminder }]);
+    const res = await fetch("http://localhost:3001/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+
+    const data = await res.json();
+    setTasks([...tasks, data]);
   };
 
   // Delete task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:3001/tasks/${id}`, { method: "DELETE" });
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
   // Toggle reminder
-  const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => (task.id === id ? { ...task, reminder: !task.reminder } : task)));
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+    const res = await fetch(`http://localhost:3001/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updatedTask),
+    });
+
+    const data = await res.json();
+    setTasks(tasks.map((task) => (task.id === id ? { ...task, reminder: data.reminder } : task)));
   };
 
+  // TASK LIST
   return (
     <div className="container">
       <Header title="Task List" onShowAddTask={toggleAddTask} addTask={showAddTasks} />
